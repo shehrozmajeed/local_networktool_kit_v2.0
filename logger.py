850b6494cc09f1fd5d319d82ae1def2bd5b22856
shehrozmajeed <165585892+shehrozmajeed@users.noreply.github.com>
2025-08-01 22:11:55 +0500

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,8 @@
+import datetime
+
+LOG_FILE = "reports/logs.txt"
+
+def log(message):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open(LOG_FILE, "a") as f:
+        f.write(f"[{timestamp}] {message}\n")
